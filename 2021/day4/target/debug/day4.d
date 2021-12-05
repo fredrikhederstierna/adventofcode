dba48992f433b5b631dd6aa3ff41fb985fec4b34
fredrikhederstierna <fredrik@hederstierna.com>
2021-12-05 02:19:38 +0100

    added day4

---
+++ 2021/day4/target/debug/day4.d
@@ -0,0 +1,1 @@
+/home/fredrik/github/adventofcode/2021/day4/target/debug/day4: /home/fredrik/github/adventofcode/2021/day4/src/main.rs
---
+++ 2021/day4/target/debug/deps/day4-6d537ecb1ac3bf2d.d
@@ -0,0 +1,5 @@
+/home/fredrik/github/adventofcode/2021/day4/target/debug/deps/day4-6d537ecb1ac3bf2d: src/main.rs
+
+/home/fredrik/github/adventofcode/2021/day4/target/debug/deps/day4-6d537ecb1ac3bf2d.d: src/main.rs
+
+src/main.rs:
