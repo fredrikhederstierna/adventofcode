9f0ceeefa4c970f3703b6df8f16797ab838ccdd9
Fredrik Hederstierna <fredrik.hederstierna@verisure.com>
2025-12-01 22:36:59 +0100

    fixes

--- 2025/day1/Main.scala
+++ 2025/day1/Main.scala
@@ -7,8 +7,10 @@ import java.util.stream.Collectors;
 class AocApp extends App :
   println("Run...")
 
+  var full_turns = 0
   var zeros = 0
   var dial = 50
+  var clicks = 0
 
   //val input = "L68 L30 R48 L5 R60 L55 L1 L99 R14 L82";
   // stdin
@@ -27,12 +29,29 @@ class AocApp extends App :
       end if
       var digits : Integer = Integer.parseInt(token.substring(1).trim())
       //println("i=" + digits)
-      dial = (dial + (sign * digits)) % 100
-      if dial < 0 then dial = dial + 100
-      //println("dial = " + dial)
+      // add full turns
+      var abs_digits : Integer = Math.abs(digits)
+      if abs_digits > 100 then
+          full_turns = full_turns + (abs_digits / 100)
+          digits = digits % 100
+      if digits > 0 then
+          var dial_new = (dial + (sign * digits))
+          //println("dial_new = " + dial_new)
+          if (dial_new < 0) && (dial > 0) then
+              clicks = clicks + 1
+              //println("neg click")
+          if (dial_new > 100) then
+              clicks = clicks + 1
+              //println("pos click")
+          dial = (dial_new % 100)
+          if dial < 0 then dial = dial + 100
+          //println("dial = " + dial)
       if dial == 0 then zeros = zeros + 1
   end while
   println("zeros = " + zeros)
+  println("full_turns = " + full_turns)
+  println("clicks = " + clicks)
+  println("zeros + full_turns + clicks = " + (zeros + full_turns + clicks))
 
   def getInput() : String =
     var reader = new BufferedReader(new InputStreamReader(System.in))
